871fa9cb82586f9786f794fc5780acc647f79153
Artur Blaya <arturblaya@gmail.com>
2025-02-08 20:51:24 +0100

    fix: Adjust Header component layout and styling for improved responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,9 @@ import Logo from "../../public/logo-mapi.svg";
 
 export default function Header() {
   return (
-    <header className="bg-primary pt-safe">
+    <header className="pt-safe bg-primary">
       <div className="container mx-auto flex max-w-screen-lg items-center justify-between">
-        <Link href="/">
+        <Link href="/" className="flex-shrink-0">
           <Image
             src={Logo}
             alt="Logo"
@@ -18,8 +18,22 @@ export default function Header() {
         <input
           type="text"
           placeholder="Cerca..."
-          className="input input-bordered input-secondary max-w-xs mr-4"
+          className="input input-bordered input-secondary mr-4 w-full max-w-80"
         />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="mr-4 size-6 flex-shrink-0 text-base-100"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+          />
+        </svg>
       </div>
     </header>
   );
