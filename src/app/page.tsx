3abaf31ac3a0d0434ebea86d3f34dfc28d50a8e1
Artur Blaya <arturblaya@gmail.com>
2025-02-08 19:32:08 +0100

    feat: Add Tailwind CSS typography plugin and custom theme; update Home page layout and remove unused fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
-export default async function Home() {
+export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-base-100">
+      <div className="prose">
+        <h1>¡Benvingut a la Carnisseria Mama Pilar!</h1>
+        <p>
+          Des de fa anys, seleccionem les millors carns amb un compromís ferm
+          amb la qualitat i la proximitat. Gaudeix de productes frescos i un
+          servei proper que ens distingeix.
+        </p>
+        <button className="btn btn-primary mt-4">Descobreix</button>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,5 +8,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#680a18",
+          "secondary": "#A64253",
+          "accent": "#D9B08C",
+          "neutral": "#3D2C2E",
+          "base-100": "#FAF3E0",
+          "info": "#6A8D92",
+          "success": "#4F7942",
+          "warning": "#D4A017",
+          "error": "#B22222",
+        },
+      },
+    ],
+  },
 };
