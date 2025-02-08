eda700ff0791837cc562707f574462a7ee29042f
Artur Blaya <arturblaya@gmail.com>
2025-02-08 18:51:14 +0100

    refactor: Remove product-related components and services; update Next.js configuration for experimental server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import ProductList from "../product/ui/ProductList";
-import { getProducts } from '@/product/services/getProducts';
-
 export default async function Home() {
-  const products = await getProducts();
-
   return (
-    <div className="min-h-screen p-4">
-      <ProductList products={products} />
+    <div className="min-h-screen">
     </div>
   );
 }
