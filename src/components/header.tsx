a23672c1de7564e18a5be0053e06047f2d852ceb
Artur Blaya <arturblaya@gmail.com>
2025-02-08 20:26:31 +0100

    feat: Add search input to Header component for improved navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import Logo from "../../public/logo-mapi.svg";
 export default function Header() {
   return (
     <header className="bg-primary">
-      <div className="container mx-auto max-w-screen-lg flex items-center justify-between">
+      <div className="container mx-auto flex max-w-screen-lg items-center justify-between">
         <Link href="/">
           <Image
             src={Logo}
@@ -15,6 +15,11 @@ export default function Header() {
             className="cursor-pointer"
           />
         </Link>
+        <input
+          type="text"
+          placeholder="Cerca..."
+          className="input input-bordered input-secondary max-w-xs mr-4"
+        />
       </div>
     </header>
   );
